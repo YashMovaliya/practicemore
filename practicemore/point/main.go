d75e1cf774cd9e042dba3266a3b8eac5bd81e8cc
yashmovaliya1607@gmail.com <yashmovaliya1607@gmail.com>
2020-10-21 17:03:32 +0530

    pointer concept

---
+++ practicemore/point/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func changeValue(str *string) {
+	*str = "changed!"
+}
+
+func changeValue2(str string) string {
+	str = "changed!"
+	return str
+}
+func main() {
+	toChange := "hello"
+	fmt.Println(toChange)
+	changeValue(&toChange)
+	//changeValue2(toChange)
+	fmt.Println(toChange)
+}
